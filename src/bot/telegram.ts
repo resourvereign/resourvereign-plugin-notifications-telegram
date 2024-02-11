a6e62a13dc1f9c38c04a6a283de119cd1aa74bc5
Dani Lupión <danilupion@gmail.com>
2024-02-11 18:49:35 +0100

    Welcome and validation flows

--- src/bot/telegram.ts
+++ src/bot/telegram.ts
@@ -1,12 +1,116 @@
-import { Telegraf } from 'telegraf';
+import { Markup, Telegraf } from 'telegraf';
+import { v4 as uuid } from 'uuid';
+
+type Action = {
+  text: string;
+  callback: () => void;
+};
+
+type ActionEntry = {
+  callback: () => void;
+  messageId: string; // Unique identifier for the message this action is associated with
+};
+
+type MessageActionsEntry = {
+  date: Date;
+  actionIds: string[];
+};
+
 export class TelegramClient {
   private bot: Telegraf;
 
+  private actions = new Map<string, ActionEntry>();
+  private messageActions = new Map<string, MessageActionsEntry>();
+  private cleaningInterval: NodeJS.Timeout;
+
   constructor(token: string) {
     this.bot = new Telegraf(token);
+
+    this.bot.command('start', (ctx) =>
+      ctx.reply(
+        `Welcome! your id is ${ctx.from.id}, you will need to provide it in the plugin configuration`,
+      ),
+    );
+
+    this.bot.action(/.+/, async (ctx) => {
+      const actionId = ctx.match[0];
+      if (this.actions.has(actionId)) {
+        const actionEntry = this.actions.get(actionId);
+        this.actions.get(actionId)?.callback();
+
+        // Cleanup: Remove all actions associated with this action's message
+        if (actionEntry && this.messageActions.has(actionEntry.messageId)) {
+          this.messageActions
+            .get(actionEntry.messageId)
+            ?.actionIds.forEach((id) => this.actions.delete(id));
+          this.messageActions.delete(actionEntry.messageId); // Remove the entry for this message
+        }
+
+        // Edit the message to disable the buttons
+        await ctx.editMessageReplyMarkup({
+          inline_keyboard: [],
+        });
+      }
+    });
+
+    // Cleanup: Remove all actions older than 24 hour (check every 5 minutes)
+    this.cleaningInterval = setInterval(
+      () => {
+        const now = new Date();
+        for (const [messageId, entry] of this.messageActions.entries()) {
+          if (now.getTime() - entry.date.getTime() > 24 * 60 * 60 * 1000) {
+            // TODO: Remove the message action in telegram
+            this.messageActions.delete(messageId);
+            entry.actionIds.forEach((id) => this.actions.delete(id));
+          }
+        }
+      },
+      5 * 60 * 1000, // Cleanup every 5 minutes
+    );
   }
 
-  async sendMessage(id: number, message: string) {
-    await this.bot.telegram.sendMessage(id, message);
+  async start() {
+    await this.bot.launch();
+  }
+
+  stop() {
+    this.messageActions.clear();
+    this.actions.clear();
+
+    clearInterval(this.cleaningInterval);
+
+    this.bot.stop();
+  }
+
+  async sendMessage({
+    userId,
+    message,
+    actions,
+  }: {
+    userId: number;
+    message: string;
+    actions?: Action[];
+  }) {
+    const messageId = uuid();
+    const actionIds: string[] = [];
+
+    const processedActions = (actions ?? []).map((action) => {
+      const actionId = uuid();
+      this.actions.set(actionId, { callback: action.callback, messageId });
+      actionIds.push(actionId);
+      return {
+        ...action,
+        uuid: actionId,
+      };
+    });
+
+    this.messageActions.set(messageId, { actionIds, date: new Date() });
+
+    await this.bot.telegram.sendMessage(userId, message, {
+      parse_mode: 'Markdown',
+      ...Markup.inlineKeyboard(
+        processedActions.map((action) => Markup.button.callback(action.text, action.uuid)),
+      ),
+    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,97 @@
 import { Logger } from '@resourvereign/plugin-types/logger.js';
-import { PluginSchemaPropertyType } from '@resourvereign/plugin-types/plugin/index.js';
-import { NotificationsPlugin } from '@resourvereign/plugin-types/plugin/notifications.js';
+import { PluginSchema } from '@resourvereign/plugin-types/plugin/index.js';
+import {
+  NotificationsPlugin,
+  ResultNotification,
+} from '@resourvereign/plugin-types/plugin/notifications.js';
+import config from 'config';
+import { format } from 'date-fns';
 
 import { TelegramClient } from './bot/telegram.js';
 
-const schema = {
+const token = config.get<string>('plugins.telegram.token');
+const validationTimeout = config.get<number>('plugins.telegram.validationTimeout');
+
+const schema: PluginSchema = {
   properties: {
-    token: {
-      type: PluginSchemaPropertyType.string,
-    },
     userId: {
-      type: PluginSchemaPropertyType.uint32,
+      type: 'uint32',
     },
   },
 };
 
 type TelegramInitData = {
-  token: string;
   userId: number;
 };
 
-const initialize = async ({ token, userId }: TelegramInitData, logger: Logger) => {
-  return {
-    validate() {
-      logger.debug(`Starting validation`);
-      return typeof token === 'string' && typeof userId === 'number' && !!token && !!userId;
-    },
-    async notify(message: string) {
-      try {
-        logger.debug(`Sending notification ${message}`);
-        const telegramClient = new TelegramClient(token);
-        await telegramClient.sendMessage(userId, message);
-        return true;
-      } catch (error) {
-        logger.error(`Failed to send notification: ${error}`);
-        return false;
-      }
-    },
+let telegramClient: TelegramClient;
+
+const register = () => {
+  telegramClient = new TelegramClient(token);
+  telegramClient.start();
+
+  return async ({ userId }: TelegramInitData, logger: Logger) => {
+    return {
+      async validate() {
+        try {
+          logger.debug(`Starting validation`);
+
+          if (!token) {
+            logger.error(`Token not found`);
+            return false;
+          }
+
+          if (!userId || typeof userId !== 'number') {
+            logger.error(`Invalid  userId`);
+            return false;
+          }
+
+          return await Promise.race([
+            new Promise<boolean>((res) => {
+              setTimeout(() => res(false), validationTimeout);
+            }),
+            new Promise<boolean>((res) => {
+              telegramClient.sendMessage({
+                userId,
+                message: 'Enable plugin?',
+                actions: [
+                  {
+                    text: 'Yes!',
+                    callback: () => {
+                      res(true);
+                    },
+                  },
+                ],
+              });
+            }),
+          ]);
+        } catch (error) {
+          logger.error(`Failed to send notification: ${error}`);
+          return false;
+        }
+      },
+      async notify(notification: ResultNotification) {
+        try {
+          const message = `*${notification.integration}:* booked *${notification.resource}* for *${format(notification.date, 'yyyy-MM-dd')}*`;
+
+          logger.debug(`Sending notification ${message}`);
+          await telegramClient.sendMessage({ userId, message });
+          return true;
+        } catch (error) {
+          logger.error(`Failed to send notification: ${error}`);
+          return false;
+        }
+      },
+    };
   };
 };
 
 export default {
   schema,
-  initialize,
+  register,
+  unregister: async () => {
+    if (telegramClient) {
+      telegramClient.stop();
+    }
+  },
 } satisfies NotificationsPlugin<TelegramInitData>;
